940a682853558d2cdb1d05fc9bb02cee89588adb
Derek <dbi208@nyu.edu>
2017-12-04 11:22:03 -0500

    Working up to Test3

--- workdir/batch/runcubep3m_nu.s
+++ workdir/batch/runcubep3m_nu.s
@@ -13,7 +13,7 @@ source /home/dbi208/.bashrc
 iload
 
 ORGDIR=$HOME/src/cubep3m_dmnu
-RUNDIR=$SCRATCH/DMNU/Test2_1/cubep3m_dmnu
+RUNDIR=$SCRATCH/DMNU/Test3/cubep3m_dmnu
 
 cp -r $ORGDIR $RUNDIR/source
 SUBDIR=$RUNDIR/source/workdir/batch
