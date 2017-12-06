79cca4b5350a2dfe962bef68057e21bf01163edd
Derek <dbi208@nyu.edu>
2017-12-06 10:27:22 -0500

    Removed DIAG from coarse_mesh

--- workdir/source_threads/coarse_mesh.f90
+++ workdir/source_threads/coarse_mesh.f90
@@ -5,35 +5,13 @@
 subroutine coarse_mesh
   implicit none
   include 'mpif.h'
-#   include "cubepm.fh"
+# include "cubepm.fh"
 
-#ifdef DIAG
-  integer(4) :: i,j,k
-  real(8) :: sumrhoc,sumrhof
-#endif
-
-   call coarse_mass
-
-#ifdef DIAG
-    sumrhof=0.0
-    sumrhoc=0.0
-    do k=1,nc_node_dim
-      do j=1,nc_node_dim
-        do i=1,nc_node_dim
-          sumrhof=sumrhof+real(rho_c(i,j,k),kind=8)
-        enddo
-      enddo
-    enddo
-    call mpi_reduce(sumrhof,sumrhoc,1,mpi_double_precision, &
-                    mpi_sum,0,mpi_comm_world,ierr)
-    if (rank == 0) write(*,*) 'sum of rho_c=',sumrhoc
-    if (rank == 0) write(*,*) 'rank', rank, 'min/max rho_c =', minval(rho_c), maxval(rho_c)
-#endif
-
-   call coarse_force
-   call coarse_force_buffer
-   call coarse_max_dt
-   if (coarse_vel_update) call coarse_velocity
+  call coarse_mass
+  call coarse_force
+  call coarse_force_buffer
+  call coarse_max_dt
+  if (coarse_vel_update) call coarse_velocity
 
 end subroutine coarse_mesh
 
