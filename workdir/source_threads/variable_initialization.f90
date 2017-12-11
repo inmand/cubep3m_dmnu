28d18b9442b771307e6d64a6a42266f06025f719
Derek <dbi208@nyu.edu>
2017-12-11 11:40:19 -0500

    Commented out NESTED flags

--- workdir/source_threads/variable_initialization.f90
+++ workdir/source_threads/variable_initialization.f90
@@ -167,12 +167,12 @@ subroutine variable_initialize
 
   if (rank == 0) write(*,*) 'finished variable init'
 
-# ifdef NESTED_OMP
+!# ifdef NESTED_OMP
   call omp_set_num_threads(cores*nested_threads)
   call omp_set_nested(.true.)
-# else
-  call omp_set_num_threads(cores)
-#endif
+!# else
+!  call omp_set_num_threads(cores)
+!#endif
   if (rank == 0) write(*,*) 'finished omp call'
 
 end subroutine variable_initialize
